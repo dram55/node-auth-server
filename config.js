887ed5e1b27b2766f7ca1b35cab593602a8a88ff
dram55 <isbister1500@hotmail.com>
2017-04-26 17:28:23 -0400

    connect to MongoDB

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  port: process.env.PORT || 3090
+  port: process.env.PORT || 3090,
+  db: {
+    port: 27017,
+    host: 'localhost',
+    name: 'authentication'
+  }
 }
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const port = require('./config').port;
+const config = require('./config');
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+const dbPort = config.db.port;
+const dbName = config.db.name;
+const dbHost = config.db.host;  
+mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`);
 
 // App Setup
 const app = express();
@@ -14,6 +21,7 @@ app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
 // Server Setup
+const port = config.port;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Listening on port:', port);
