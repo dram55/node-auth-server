cb10122c1965c435e669e2696c5172cf2149b39c
dram55 <isbister1500@hotmail.com>
2017-04-27 13:23:24 -0400

    encrypt password on save

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 // Define the user schema
@@ -7,6 +8,20 @@ const userSchema = new Schema({
   email: {type: String, required: true, unique: true, lowercase: true},
   password: {type: String, required: true}
 });
+
+userSchema.pre('save', function(next){
+  const user = this;
+  bcrypt.genSalt(10,function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hashedPassword) {
+      console.log('in bcrypt hash');
+      console.log(user.password, salt, hashedPassword);
+      if (err) return next(err);
+      user.password = hashedPassword;
+      next();
+    });
+  });
+}); 
 
 // Create a user Model class
 const User = mongoose.model('user', userSchema);
